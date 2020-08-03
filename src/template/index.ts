8a3d5449d1a1e3f055ca8730d37301fd00e37b4c
alexander.burkov <alexander.burkov@jetbrains.com>
2020-08-03 17:43:22 +0300

    template command

--- src/template/index.ts
+++ src/template/index.ts
@@ -1,5 +1,17 @@
+import execa from 'execa';
+
 const template = () => {
-  console.log('template');
+  execa.sync('npm', [
+    'i',
+    '-D',
+    'typesync',
+    'typescript',
+    'npm-check-updates',
+    'ts-node',
+    'prettier',
+    'rimraf',
+  ]);
+  execa.sync('npm', ['i', 'axios', 'lodash']);
 };
 
 export default template;
