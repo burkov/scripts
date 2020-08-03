a14dfc9e612121a2c2e9f8c5ed646d89fffd43e5
alexander.burkov <alexander.burkov@jetbrains.com>
2020-08-03 17:51:26 +0300

    Use listr

--- src/template/index.ts
+++ src/template/index.ts
@@ -1,17 +1,40 @@
 import execa from 'execa';
+import Listr from 'listr';
 
 const template = () => {
-  execa.sync('npm', [
-    'i',
-    '-D',
-    'typesync',
-    'typescript',
-    'npm-check-updates',
-    'ts-node',
-    'prettier',
-    'rimraf',
+  const tasks = new Listr([
+    {
+      title: 'Installing dev packages',
+      task: () =>
+        execa('npm', [
+          'i',
+          '-D',
+          'typesync',
+          'typescript',
+          'npm-check-updates',
+          'ts-node',
+          'prettier',
+          'rimraf',
+        ]),
+    },
+    {
+      title: 'Installing core packages',
+      task: () => execa('npm', ['i', 'axios', 'lodash']),
+    },
+    {
+      title: 'Initializing TypeScript configuration',
+      task: () => execa('npx', ['tsc', '--init']),
+    },
+    {
+      title: 'Syncing types',
+      task: () => execa('npx', ['typesync']),
+    },
+    {
+      title: 'Installing packages with typings',
+      task: () => execa('npm', ['install']),
+    },
   ]);
-  execa.sync('npm', ['i', 'axios', 'lodash']);
+  tasks.run().catch(console.error)
 };
 
 export default template;
