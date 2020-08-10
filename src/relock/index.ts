3e8ae78da897217560a3b042f8fbb464ebf5f48e
alexander.burkov <alexander.burkov@jetbrains.com>
2020-08-10 14:57:11 +0300

    use listr

--- src/relock/index.ts
+++ src/relock/index.ts
@@ -2,24 +2,46 @@ import shelljs from 'shelljs';
 import _ from 'lodash';
 import { findNpmRoots } from '../common/find-npm-roots';
 import { confirmIfMoreThanOnePath } from '../common/paths-renderer';
+import Listr from 'listr';
+import execa from 'execa';
+import path from 'path';
+import chalk from 'chalk';
 
 shelljs.config.fatal = true;
 
 const relock = async () => {
   const paths = await findNpmRoots();
-  if (await confirmIfMoreThanOnePath('Re-locking following paths:', paths)) {
+  if (await confirmIfMoreThanOnePath('Relocking following paths:', paths)) {
     const pwd = shelljs.pwd().stdout;
-    _.forEach(paths, (path: string) => {
-      console.log(`Re-locking ${path}`);
-      shelljs.cd(pwd);
-      shelljs.cd(path);
-      console.log('  removing node_modules');
-      shelljs.rm('-rf', 'node_modules');
-      console.log('  removing package-lock.json');
-      shelljs.rm('-f', 'package-lock.json');
-      console.log('  running npm install');
-      shelljs.exec(`npm i`, { silent: true, fatal: true });
-    });
+    for (const p of paths) {
+      const fullPath = path.normalize(path.join(pwd, p));
+      console.log(`Re-locking ${chalk.blue(fullPath)}`);
+      const tasks = new Listr([
+        {
+          title: '  removing node_modules',
+          task: () => {
+            console.log(execa('ls', ['-l'], { cwd: fullPath }).stdout);
+            // shelljs.rm('-rf', 'node_modules');
+            return Promise.resolve();
+          },
+        },
+        {
+          title: '  removing package-lock.json',
+          task: () => {
+            // shelljs.rm('-f', 'package-lock.json');
+            return Promise.resolve();
+          },
+        },
+        {
+          title: '  running npm install',
+          task: () => {
+            console.log(fullPath);
+            //execa('npm', ['install'])
+          },
+        },
+      ]);
+      await tasks.run();
+    }
   }
 };
 
