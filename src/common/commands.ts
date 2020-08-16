28508d0d8ab6aab18d59ef5eb3f733085aa50b9c
alexander.burkov <alexander.burkov@jetbrains.com>
2020-08-16 04:42:53 +0300

    wip

---
+++ src/common/commands.ts
@@ -0,0 +1,26 @@
+import execa from 'execa';
+
+export const runTypesync = (cwd: string) => ({
+  title: 'syncing types',
+  task: () => execa('npx', ['typesync'], { cwd }),
+});
+
+export const removeNodeModuleDir = (cwd: string) => ({
+  title: 'removing node_modules',
+  task: () => execa('rm', ['-r', '-f', 'node_modules'], { cwd }),
+});
+
+export const removePackageLock = (cwd: string) => ({
+  title: 'removing package-lock.json',
+  task: () => execa('rm', ['-f', 'package-lock.json'], { cwd }),
+});
+
+export const runNpmInstall = (cwd: string) => ({
+  title: 'running npm install',
+  task: () => execa('npm', ['install'], { cwd }),
+});
+
+export const runCheckUpdates = (cwd: string) => ({
+  title: 'update package versions',
+  task: () => execa('npx', ['npm-check-updates', '-usdf'], { cwd }),
+});
--- src/up/index.ts
+++ src/up/index.ts
@@ -1,32 +1,7 @@
-import execa from 'execa';
 import path from 'path';
-import { forEachConfirmedPath } from '../common/find-npm-projects';
-import { pathColorFn } from '../common/colors';
-
-export const runTypesync = (cwd: string) => ({
-  title: 'syncing types',
-  task: () => execa('npx', ['typesync'], { cwd }),
-});
-
-export const removeNodeModuleDir = (cwd: string) => ({
-  title: 'removing node_modules',
-  task: () => execa('rm', ['-r', '-f', 'node_modules'], { cwd }),
-});
-
-export const removePackageLock = (cwd: string) => ({
-  title: 'removing package-lock.json',
-  task: () => execa('rm', ['-f', 'package-lock.json'], { cwd }),
-});
-
-export const runNpmInstall = (cwd: string) => ({
-  title: 'running npm install',
-  task: () => execa('npm', ['install'], { cwd }),
-});
-
-export const runCheckUpdates = (cwd: string) => ({
-  title: 'update package versions',
-  task: () => execa('npx', ['npm-check-updates', '-u'], { cwd }),
-});
+import {forEachConfirmedPath} from '../common/find-npm-projects';
+import {pathColorFn} from '../common/colors';
+import {removeNodeModuleDir, removePackageLock, runCheckUpdates, runNpmInstall, runTypesync,} from '../common/commands';
 
 export const upTypes = () =>
   forEachConfirmedPath(
