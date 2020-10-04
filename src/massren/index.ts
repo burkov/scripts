5356c3e2fe7309a801dc217989a1be38841964a9
alexander.burkov <alexander.burkov@jetbrains.com>
2020-10-04 22:03:47 +0300

    dir renamer

--- src/massren/index.ts
+++ src/massren/index.ts
@@ -2,8 +2,29 @@
 
 import commander from 'commander';
 import * as process from 'process';
+import { fdir as FDir } from 'fdir';
+import _ from 'lodash';
+import * as fs from 'fs';
 
-commander.option('-y', '--yes', 'do actual rename');
-commander.parse(process.argv);
+const program = new commander.Command();
 
-console.log('hello');
+program.option('-y, --yes', 'do actual rename');
+program.parse(process.argv);
+
+const crawlDir = '.';
+
+const result = new FDir()
+  .withDirs()
+  .withMaxDepth(2)
+  .filter(_.stubFalse)
+  .crawl(crawlDir)
+  .sync() as string[];
+
+const toMove = result
+  .filter((e) => /\d{4}\.\d{2}\.\d{2}/.test(e))
+  .map((from) => ({ from, to: from.replace(/\d{4}\./, '') }));
+
+toMove.forEach(({ from, to }) => {
+  console.log(`${from.replace(crawlDir, '')} -> ${to.replace(crawlDir, '')}`);
+  if (program.yes) fs.renameSync(from, to);
+});
